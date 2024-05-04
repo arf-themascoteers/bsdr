9daafd9db477374dfee38d001089ae886f1f113d
Arif <arif@themascoteers.com>
2024-05-04 10:30:30 +1000

    Start

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+from evaluator import Evaluator
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    tasks = []
+    x = [(False, False), (True, False), (False, True)]
+    for reduced_features, reduced_rows in x:
+        for algorithm in ["mi","sfs","lasso","fsdr"]:
+            for size in [2, 5, 10, 15, 20]:
+                tasks.append(
+                    {
+                        "reduced_features":reduced_features,
+                        "reduced_rows":reduced_rows,
+                        "target_size": size,
+                        "algorithm": algorithm
+                    }
+                )
+    ev = Evaluator(tasks)
+    ev.evaluate()
