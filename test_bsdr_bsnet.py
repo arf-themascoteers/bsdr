c07c6badad932a2938cda63b25120a01d4b33b56
arif <arif@themascoteers.com>
2024-05-05 10:58:20 +1000

    start

--- test_bsdr_bsnet.py
+++ test_bsdr_bsnet.py
@@ -3,7 +3,7 @@ from evaluator import Evaluator
 if __name__ == '__main__':
     tasks = {
         "algorithms" : ["bsdr","bsnet"],
-        "datasets" : ["lucas_full","lucas_downsampled"],
+        "datasets" : ["lucas_full"],
         "target_sizes" : [5, 10, 15, 20, 25, 30]
     }
     ev = Evaluator(tasks,1,10,"bsnet.csv",skip_all_bands=True)
