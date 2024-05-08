93c1b172381e59a67f01501732621c7b6aedd63d
Arif <arif@themascoteers.com>
2024-05-09 03:14:33 +1000

    Start

--- helpers/mt_plot2b.py
+++ helpers/mt_plot2b.py
@@ -2,20 +2,27 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv("combo_2b_filtered.csv")
-
 plt.figure(figsize=(8, 6))
 scatter = plt.scatter(df['band1'], df['band2'], c=df['score'], cmap='viridis', s=15)  # Decreased marker size
 cb = plt.colorbar(scatter, label='Score', orientation='vertical')
-#cb.set_label(label='$R^2$', rotation=0, labelpad=20, horizontalalignment='center', verticalalignment='top', fontsize=20)
-# Setting label to an empty string
 cb.set_label('')
 cb.ax.set_title('$R^2$', loc='left', pad=10, fontsize=20)
-
-#ax.annotate('Global maximum', (58, 59), textcoords="offset points", xytext=(0,10), ha='center', fontsize=10, color='red')
-
-
 plt.xlabel('Band Index 1', fontsize=20)
 plt.ylabel('Band Index 2', fontsize=20)
 plt.xticks(fontsize=12)
 plt.yticks(fontsize=12)
+
+row = df[(df['band1']==58) & (df['band2']==59)]
+value = round(row.iloc[0]['score'],2)
+plt.scatter(58, 59, color='red', s=50, label=f'Global Maximum: {value:0.2f}')
+
+row = df[(df['band1']==7) & (df['band2']==14)]
+value = round(row.iloc[0]['score'],2)
+plt.scatter(7, 14, color='magenta', s=50, label=f'Local Maximum 1: {value:0.2f}')
+
+row = df[(df['band1']==47) & (df['band2']==54)]
+value = round(row.iloc[0]['score'],2)
+plt.scatter(47, 54, color='coral', s=50, label=f'Local Maximum 2: {value:0.2f}')
+
+plt.legend(loc='lower right', markerscale=0.5)
 plt.savefig("fig1.png")
