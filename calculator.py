5b35c1595de61a57a2274e679c7c0eb5b23696ae
Arif <arif@themascoteers.com>
2024-05-09 01:59:19 +1000

    Start

--- calculator.py
+++ calculator.py
@@ -6,14 +6,14 @@ import math
 
 
 def calculate_r2(y, y_hat, scaler):
-    y = scaler.inverse_transform(y)
-    y_hat = scaler.inverse_transform(y_hat)
+    y = scaler.inverse_transform(y.reshape(-1, 1)).reshape(-1)
+    y_hat = scaler.inverse_transform(y_hat.reshape(-1, 1)).reshape(-1)
     r2 = r2_score(y, y_hat)
     return r2
 
 
 def calculate_rmse(y, y_hat, scaler):
-    y = scaler.inverse_transform(y)
-    y_hat = scaler.inverse_transform(y_hat)
+    y = scaler.inverse_transform(y.reshape(-1, 1)).reshape(-1)
+    y_hat = scaler.inverse_transform(y_hat.reshape(-1, 1)).reshape(-1)
     rmse = math.sqrt(mean_squared_error(y, y_hat))
     return rmse
