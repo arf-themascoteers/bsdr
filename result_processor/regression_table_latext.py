afe8a43f3bca33496cb871482d7dccb26ccbfe6e
Arif <arif@themascoteers.com>
2024-05-09 18:18:31 +1000

    Start

--- result_processor/regression_table_latext.py
+++ result_processor/regression_table_latext.py
@@ -1,1 +1,12 @@
 import pandas as pd
+
+time_df = pd.read_csv("../final_results/regression_time.csv")
+r2_df = pd.read_csv("../final_results/regression_r2.csv")
+rmse_df = pd.read_csv("../final_results/regression_rmse.csv")
+
+priority_order = ['PCA-loading', 'LASSO', 'MCUVE',
+                  #'SPA',
+                  'BS-Net-FC', 'BSDR','All Bands']
+# r2_df['algorithm'] = pd.Categorical(r2_df['algorithm'], categories=priority_order, ordered=True)
+# r2_df = r2_df.sort_values('algorithm')
+print(r2_df)
