faa76c56fb5703d1ec6f856bbee96c3cfdd8c4f0
Arif <arif@themascoteers.com>
2024-05-09 18:03:45 +1000

    Start

--- test_1.py
+++ test_1.py
@@ -2,7 +2,7 @@ from task_runner import TaskRunner
 
 if __name__ == '__main__':
     tasks = {
-        "algorithms" : ["pcal","lasso","mcuve","spa","bsnet","zhang","bsdr"],
+        "algorithms" : ["pcal","lasso","mcuve","spa","bsnet","zhang"],
         "datasets" : ["ghisaconus","indian_pines"],
         "target_sizes" : [5, 10, 15, 20, 25, 30]
     }
--- test_2.py
+++ test_2.py
@@ -2,7 +2,7 @@ from task_runner import TaskRunner
 
 if __name__ == '__main__':
     tasks = {
-        "algorithms" : ["pcal","lasso","mcuve","bsnet","bsdr"],
+        "algorithms" : ["pcal","lasso","mcuve","bsnet"],
         "datasets" : ["lucas"],
         "target_sizes" : [5, 10, 15, 20, 25, 30]
     }
