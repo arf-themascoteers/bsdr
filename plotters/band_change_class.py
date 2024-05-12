83519794a0f69dfaff7968ba503b78cb178734a2
Arif <arif@themascoteers.com>
2024-05-13 02:06:59 +1000

    Start

--- plotters/band_change_class.py
+++ plotters/band_change_class.py
@@ -19,7 +19,7 @@ for dataset in datasets.keys():
         loc = os.path.join(root, file)
         if not os.path.exists(loc):
             continue
-        fig, ax = plt.subplots(1,1, figsize=(10, 6))
+        fig, ax = plt.subplots(1,1, figsize=(10, 8))
         df = pd.read_csv(loc)
         band_labels = []
         for i in range(1,target_size+1):
@@ -29,17 +29,17 @@ for dataset in datasets.keys():
         cols_to_increment = df2.columns.difference(['epoch'])
         df2[cols_to_increment] = df2[cols_to_increment].add(1)
         for i in range(1,target_size+1):
-            ax.plot(df2["epoch"], df2[f"band_{i}"], label=f"Band Index {i}")
-        ax.set_xlabel('Epoch', fontsize=18)
-        ax.set_ylabel('Band Index', fontsize=18)
-        ax.tick_params(axis='both', which='major', labelsize=14)
+            ax.plot(df2["epoch"], df2[f"band_{i}"], label=f"Target Index {i}")
+        ax.set_xlabel('Epoch', fontsize=25)
+        ax.set_ylabel('Band Index', fontsize=25)
+        ax.tick_params(axis='both', which='major', labelsize=20)
         ax.set_xlim(0, 500)
         ax.set_ylim(1, datasets[dataset]+1)
         plt.gca().spines['top'].set_visible(False)
         plt.gca().spines['right'].set_visible(False)
         plt.gca().spines['left'].set_visible(False)
         plt.gca().spines['bottom'].set_visible(False)
-        ax.legend(loc='upper right', ncol=5, bbox_to_anchor=(1, 1.1))
+        ax.legend(loc='upper right', ncol=3, bbox_to_anchor=(1.1, 1.15), fontsize=20)
         subfolder = os.path.join("../saved_figs", "bands")
         if not os.path.exists(subfolder):
             os.mkdir(subfolder)
